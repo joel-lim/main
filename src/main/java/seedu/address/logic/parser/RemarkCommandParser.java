2f124b6d844005c8d3354dd80fd9b4c1436f1ed1
joel-lim <joellimaojun@hotmail.com>
2020-02-17 00:03:50 +0800

    Add Remark to model

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -7,6 +7,8 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
+
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 
 /**
@@ -32,7 +34,7 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
                 RemarkCommand.MESSAGE_USAGE), ive);
         }
     
-        String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+        Remark remark = new Remark(argMultimap.getValue(PREFIX_REMARK).orElse(""));
     
         return new RemarkCommand(index, remark);
     }
